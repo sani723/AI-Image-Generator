c88a04d0557bddd7056abf48f50edc64e69742a1
Sajjad Haider <sani723@hotmail.com>
2023-12-21 23:58:16 +0400

    Add Image Generator form and its related styles

--- src/components/ImageGenerator/index.js
+++ src/components/ImageGenerator/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './index.css';
+import default_image from "../../assets/images/gen-ai.webp";
 
 const ImageGenerator = () => {
   return (
-    <div>index</div>
+    <div className='ai-image-generator'>
+        <div className='header'>Ai image <span>generator</span></div>
+        <div className='img-loading'>
+            <div className='image'>
+                <img src={default_image} alt='' />
+            </div>
+        </div>
+        <div className='search-box'>
+            <input type='text' className='input-search' placeholder='Describe your image' />
+            <div className='btn-generate'>Generate</div>
+        </div>
+    </div>
   )
 }
 
